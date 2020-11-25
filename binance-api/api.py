e2a8d73f17dd8286959182385539745f8f913fcb
wancat <wancat@pm.me>
2020-11-25 14:18:08 +0800

    研究 API 回傳值
    - 成交數量為 commulativeQuoteQty （扣除手續費）
    - 操作的幣值為 executedQty
    - side 紀錄買或是賣
    - 限價的時候 price 為價格，市價則為 0
    - type 紀錄市價或限價 LIMIT or MARKET

--- binance-api/api.py
+++ binance-api/api.py
@@ -1,10 +1,12 @@
+from enum import Enum, auto
 from binance.client import Client
 from os import environ
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 
-class BinanceClient:
+class Binance:
     client = Client
     def __init__(self, api_key, api_secret):
         self.client = Client(api_key, api_secret)
@@ -13,6 +15,26 @@ class BinanceClient:
     def get_orders(self, symbol: str):
         return self.client.get_all_orders(symbol=symbol, limit=10)
 
+class Currency(Enum):
+    BTC = "BTC"
+    ETH = "ETH"
+    TWD = "TWD"
+    USD = "USD"
+    DAI = "DAI"
+    USDC = "USDC"
+    USDT = "USDT"
+
+    @classmethod
+    def choices(cls):
+        result = tuple((i.name+j.name, i.name+j.name) for i in cls for j in cls if i != j)
+        print(result)
+        return result
+
+class Record:
+    id: int = 0
+    market: Currency = ""
+    
+
 if __name__ == "__main__":
-    client = BinanceClient(environ.get("API_KEY"), environ.get("API_SECRET"))
-    print(client.get_orders("BTCUSDC"))
+    client = Binance(environ.get("API_KEY"), environ.get("API_SECRET"))
+    print([(v["price"], v["side"], v["executedQty"], v["cummulativeQuoteQty"], v["type"]) for v in client.get_orders(sys.argv[1]) if v['status'] == 'FILLED'])
--- web/models.py
+++ web/models.py
@@ -1,21 +1,6 @@
 from django.db import models
-from enum import Enum, auto
 # Create your models here.
 
-class Currency(Enum):
-    BTC = "BTC"
-    ETH = "ETH"
-    TWD = "TWD"
-    USD = "USD"
-    DAI = "DAI"
-    USDC = "USDC"
-    USDT = "USDT"
-
-    @classmethod
-    def choices(cls):
-        result = tuple((i.name+j.name, i.name+j.name) for i in cls for j in cls if i != j)
-        print(result)
-        return result
 
 class Record(models.Model):
     SELL = "SELL"
