27df3c3286e37a3d3dbfaea96a981076fcdf2b72
wancat <wancat@pm.me>
2020-11-25 14:59:53 +0800

    設計 Record class
    將交易紀錄存在 Record obj 中
    建立取得使用者財產清單的函數（請不要把小額代幣換為 BNB）
    
    正在想辦法過濾出所有交易兌

--- binance-api/api.py
+++ binance-api/api.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from enum import Enum, auto
 from binance.client import Client
 from os import environ
@@ -8,33 +9,48 @@ load_dotenv()
 
 class Binance:
     client = Client
-    def __init__(self, api_key, api_secret):
+    def __init__(self, api_key: str, api_secret: str):
         self.client = Client(api_key, api_secret)
-        print(self.client.get_server_time())
 
     def get_orders(self, symbol: str):
-        return self.client.get_all_orders(symbol=symbol, limit=10)
+        data = self.client.get_all_orders(symbol=symbol, limit=10)
+        return [Record(v) for v in data if v['status'] == 'FILLED']
 
-class Currency(Enum):
-    BTC = "BTC"
-    ETH = "ETH"
-    TWD = "TWD"
-    USD = "USD"
-    DAI = "DAI"
-    USDC = "USDC"
-    USDT = "USDT"
+    def get_balances(self):
+        info = self.client.get_account()
+        return info['balances']
+    
+    def assets(self):
+        return [v['asset'] for v in self.get_balances() if float(v['free']) + float(v['locked']) != 0]
 
-    @classmethod
-    def choices(cls):
-        result = tuple((i.name+j.name, i.name+j.name) for i in cls for j in cls if i != j)
-        print(result)
-        return result
+def all_symbols(currencies):
+    result = tuple(f"{i}{j}" for i in currencies for j in currencies if i != j)
+    print(result)
+    return result
 
 class Record:
     id: int = 0
-    market: Currency = ""
+    side: bool = False
+    symbol: str = ""
+    executed_qty: float = 0
+    cummulative_quote_qty: float = 0
+
+    def __init__(self, table: Dict[str, str]):
+        self.id = int(table["orderId"])
+        self.symbol = table["symbol"]
+        self.executed_qty = float(table["executedQty"])
+        self.cummulative_quote_qty = float(table["cummulativeQuoteQty"])
+
+
+    def price(self) -> float:
+        return self.cummulative_quote_qty / self.executed_qty
     
+    def __str__(self):
+        return "id: {}, symbol: {}, executedQty: {}, quote: {}, price: {}".format(self.id, self.symbol, self.executed_qty, self.cummulative_quote_qty, self.price())
+    
+my_currencies = ['BTC', 'ETH', 'LTC', 'EOS', 'XRP', 'USDC', 'USDT']
 
 if __name__ == "__main__":
     client = Binance(environ.get("API_KEY"), environ.get("API_SECRET"))
-    print([(v["price"], v["side"], v["executedQty"], v["cummulativeQuoteQty"], v["type"]) for v in client.get_orders(sys.argv[1]) if v['status'] == 'FILLED'])
+    print(client.assets())
+    print([str(v) for v in client.get_orders(sys.argv[1])])
