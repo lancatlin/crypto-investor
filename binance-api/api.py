e450c09b245661b8440edd812cde92efaf8303e5
wancat <wancat@pm.me>
2020-11-25 16:36:34 +0800

    use yield

--- binance-api/api.py
+++ binance-api/api.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Generator
 from enum import Enum, auto
 from binance.client import Client # type: ignore
 from os import environ
@@ -23,17 +23,17 @@ class Binance:
         return [v['asset'] for v in self.client.get_account()['balances'] \
             if float(v['free']) + float(v['locked']) != 0]
 
-    def symbols(self) -> List[str]:
+    def symbols(self) -> Generator[str, None, None]:
         if self.__symbols is not None:
             return self.__symbols
-        return [v for v in all_symbols(self.assets()) \
-            if self.client.get_symbol_info(v) is not None]
+        for v in all_symbols(self.assets()):
+            if self.client.get_symbol_info(v) is not None:
+                yield v
 
-    def records(self) -> List[Record]:
-        records: List[Record] = []
+    def records(self) -> Generator[Record, None, None]:
         for symbol in self.symbols():
-            records += self.get_orders(symbol)
-        return records
+            for r in self.get_orders(symbol):
+                yield r
 
 
 def all_symbols(currencies: List[str]):
