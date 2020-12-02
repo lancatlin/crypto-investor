29e06c278747da4d9fed95709678ac6afc854591
wancat <wancat@pm.me>
2020-12-02 13:24:30 +0800

    Add symbols list cache

--- web/binance_client.py
+++ web/binance_client.py
@@ -3,14 +3,14 @@ from binance.client import Client # type: ignore
 from web.models import Record
 
 class Binance:
-    client = Client
+    client: Client = None
     __symbols: Optional[List[str]] = None
 
     def __init__(self, api_key: Optional[str], api_secret: Optional[str]):
         self.client = Client(api_key, api_secret)
 
     def get_orders(self, symbol: str):
-        data = self.client.get_all_orders(symbol=symbol, limit=10)
+        data = self.client.get_all_orders(symbol=symbol)
         return [Record(v) for v in data if v['status'] == 'FILLED']
 
     def assets(self) -> List[str]:
@@ -19,10 +19,15 @@ class Binance:
 
     def symbols(self) -> Generator[str, None, None]:
         if self.__symbols is not None:
-            return self.__symbols
+            for symbol in self.__symbols:
+                yield symbol
+            return
+        symbols = []
         for v in all_symbols(self.assets()):
             if self.client.get_symbol_info(v) is not None:
+                symbols.append(v)
                 yield v
+        self.__symbols = symbols
 
     def records(self) -> Generator[Record, None, None]:
         for symbol in self.symbols():
--- web/binance_test.py
+++ web/binance_test.py
@@ -4,13 +4,14 @@ from dotenv import load_dotenv
 from os import environ
 
 class BinanceTestCase(TestCase):
+    client: Binance = None
     def setUp(self):
         load_dotenv()
         api_key = environ.get("API_KEY")
         api_secret = environ.get("API_SECRET")
-        print(f"{api_key}, {api_secret}")
         self.client = Binance(api_key, api_secret)
     
     def test_find_records(self):
         for record in self.client.records():
             print(record)
+        print([i for i in self.client.symbols()])
