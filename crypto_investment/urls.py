52437422f8009d6ee3c9c307d7576e7814629e15
wancat <wancat@pm.me>
2020-12-09 14:13:29 +0800

    Add login required to record list page

--- crypto_investment/urls.py
+++ crypto_investment/urls.py
@@ -1,20 +1,6 @@
-"""crypto_investment URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from web.views import index, login, logout, reload_orders, RecordList
 
 urlpatterns = [
@@ -23,5 +9,5 @@ urlpatterns = [
     path('login', login),
     path('logout', logout),
     path('reload', reload_orders),
-    path('records', RecordList.as_view()),
+    path('records', login_required(RecordList.as_view())),
 ]
